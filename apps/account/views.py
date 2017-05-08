fdde6dc0b02260b326dec72dd08ae7cf919c9e8a
Jorgito <jorge.calvo@tektonlabs.com>
2017-05-07 20:54:57 -0500

    refactor linked accounts

--- apps/account/views.py
+++ apps/account/views.py
@@ -19,12 +19,20 @@ class APILinkedAccountList(APIView):
 			user = User.objects.get(username = request.data['username'])
 		except User.DoesNotExist:
 			user = None
+		try:
+			linked_account = LinkedAccount.objects.filter(twitter_user_id = request.data['linked_account_params']['twitter_user_id'], twitter_token = request.data['linked_account_params']['twitter_token']).first()
+		except LinkedAccount.DoesNotExist:
+			linked_account = None
 		if user is not None:
-			linked_account_json = LinkedAccountSerializer(data = request.data['linked_account_params'])
-			if linked_account_json.is_valid():
-				linked_account = linked_account_json.save()
+			if linked_account is not None:
 				linked_account.users.add(user)
-				return Response(linked_account_json.data, status = 201)
-			return Response(linked_account_json.errors, status = 400)
+				return Response({"success": True}, status = 200)
+			else:
+				linked_account_json = LinkedAccountSerializer(data = request.data['linked_account_params'])
+				if linked_account_json.is_valid():
+					linked_account = linked_account_json.save()
+					linked_account.users.add(user)
+					return Response(linked_account_json.data, status = 201)
+				return Response(linked_account_json.errors, status = 400)
 		else:
 			return Response({"user": "invalid username"})

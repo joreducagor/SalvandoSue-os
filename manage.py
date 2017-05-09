b00c2a370e05f2725f3d60e58f03f52da11e8e61
jc3000 <jorge.calvo@tektonlabs.com>
2017-05-09 17:35:38 -0500

    change name

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SalvandoSueños.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salvando_suenos.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
--- salvando_suenos/wsgi.py
+++ salvando_suenos/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for SalvandoSueños project.
+WSGI config for salvando_suenos project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SalvandoSueños.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "salvando_suenos.settings")
 
 application = get_wsgi_application()

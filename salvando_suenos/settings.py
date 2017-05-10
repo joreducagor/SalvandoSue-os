47cfe13ae5109c78d9e9b16a6c5386fcd88b9dfa
jc3000 <jorge.calvo@tektonlabs.com>
2017-05-10 09:33:04 -0500

    configurations

--- salvando_suenos/settings.py
+++ salvando_suenos/settings.py
@@ -22,6 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY='uf$hmc)97az@6tpi3s!n!j7@)&mt-9od%_c4*3pd5e^ax1^1u1'
 DEBUG=True
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['salvandosuenos-app.herokuapp.com']
 

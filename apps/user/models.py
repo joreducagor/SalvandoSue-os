dae229877ebc618514dc7babd223d66f5a7d4d7f
jc3000 <jorge.calvo@tektonlabs.com>
2017-05-10 16:52:19 -0500

    adding nullable to user fields

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class DetailUser(models.Model):
 	twitter_token = models.CharField(max_length = 50)
-	location = models.CharField(max_length = 100)
-	description = models.TextField(default = '')
+	location = models.CharField(max_length = 100, null = True, blank = True)
+	description = models.TextField(default = '', null = True, blank = True)
 	enabled = models.BooleanField(default = True)
 	created_at = models.DateTimeField(auto_now = True)
 	user = models.OneToOneField(User, null = False, blank = False, on_delete = models.CASCADE)

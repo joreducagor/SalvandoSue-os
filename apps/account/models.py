1136f9256b5915f932f54242a4ddd80e6838c43b
jc3000 <jorge.calvo@tektonlabs.com>
2017-05-15 17:47:26 -0500

    adding screen name to linked accounts

--- apps/account/models.py
+++ apps/account/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from apps.user.models import User
 
 class LinkedAccount(models.Model):
-	twitter_user_id = models.CharField(max_length = 50)
-	twitter_token = models.CharField(max_length = 50, null = True, blank = True)
-	created_at = models.DateTimeField(auto_now = True)
-	users = models.ManyToManyField(User)
+  twitter_user_id = models.CharField(max_length = 50)
+  twitter_token = models.CharField(max_length = 50, null = True, blank = True)
+  twitter_screen_name = models.CharField(max_length = 50, null = True, blank = True)
+  created_at = models.DateTimeField(auto_now = True)
+  users = models.ManyToManyField(User)
 
-	class Meta:
-		verbose_name = "Account"
-		verbose_name_plural = "Accounts"
+  class Meta:
+    verbose_name = "Account"
+    verbose_name_plural = "Accounts"
 
-	def __str__(self):
-		return (self.twitter_user_id)
+  def __str__(self):
+    return (self.twitter_user_id)

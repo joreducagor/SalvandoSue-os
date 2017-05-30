9e484fa1bffd018f94dec67f8777ba8b2c51a544
jc3000 <jorge.calvo@tektonlabs.com>
2017-05-29 20:09:45 -0500

    refactor tasks on linked accounts

--- apps/account/tasks.py
+++ apps/account/tasks.py
@@ -19,29 +19,30 @@ api = tweepy.API(auth)
 
 @periodic_task(run_every = (crontab(minute = '*/10')), ignore_result = True)
 def linked_accounts_analyzer():
-	#for linked_account in LinkedAccount.objects.all():
-	linked_account = LinkedAccount.objects.last()
-	#try:
-	user = api.get_user(linked_account.twitter_screen_name)
-	tweets = api.user_timeline(screen_name = linked_account.twitter_screen_name, count = 20)
-	results = []
-	for tweet in tweets:
-		results.append(tweet.text)
-	if linked_account.last_tweet in results:
-		index = results.index(linked_account.last_tweet)
-		results = results[:index]
-	if results:
-		linked_account.last_tweet = results[0]
-		linked_account.save()
-		logger.info("Text considered dangerous")
-		logger.info("--------------------------------------")
-		for result in results:
-			analized_text = TextBlob(str(result)).translate(to = 'en')
-			if analized_text.sentiment.polarity < 0:
-				logger.info(result + " ---- polarity: " + str(analized_text.sentiment.polarity))
-		logger.info("--------------------------------------")
+	for linked_account in LinkedAccount.objects.all():
+		users = linked_account.users.all()
+		if users:
+			try:
+				user = api.get_user(linked_account.twitter_screen_name)
+				tweets = api.user_timeline(screen_name = linked_account.twitter_screen_name, count = 20)
+				results = []
+				for tweet in tweets:
+					results.append(tweet.text)
+				if linked_account.last_tweet in results:
+					index = results.index(linked_account.last_tweet)
+					results = results[:index]
+				if results:
+					linked_account.last_tweet = results[0]
+					linked_account.save()
+					logger.info("Text considered dangerous")
+					logger.info("--------------------------------------")
+					for result in results:
+						analized_text = TextBlob(str(result)).translate(to = 'en')
+						if analized_text.sentiment.polarity < 0:
+							logger.info(result + " ---- polarity: " + str(analized_text.sentiment.polarity))
+					logger.info("--------------------------------------")
 
-	#except tweepy.TweepError as e:
-	#	logger.info("Internal problems with the linked accounts")
+			except tweepy.TweepError as e:
+				logger.info("Internal problems with the linked accounts")
 
 	name = "linked_accounts_analyzer"
